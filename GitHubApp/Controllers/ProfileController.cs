37b2b542883b5ef2eab362aa0b90c25ef07af7c5
MACAW\Erikas <erikas.pomeliaika@macaw.nl>
2022-09-15 17:00:55 +0300

    Fixed bug on profile page

--- GitHubApp/Controllers/ProfileController.cs
+++ GitHubApp/Controllers/ProfileController.cs
@@ -14,45 +14,58 @@ namespace GitHubApp.Controllers
         private const string EMPTYFIELD = "This field is empty! Please fill in the field!";
         private const string USERAGENT = "User-Agent";
         private const string REQUEST = "request";
+        private HttpClient gitHubClient;
+
+        public ProfileController()
+        {
+            gitHubClient = new HttpClient();
+            gitHubClient.BaseAddress = new Uri(GITHUBUSERSURL);
+            gitHubClient.DefaultRequestHeaders.Add(USERAGENT, REQUEST);
+        }
 
         ///Calls GitHub user profile API returns model with Index view 
         public ActionResult Profile(string GitHubUsername)
         {
             ProfileModel model = new ProfileModel();
-
+            model.StatusCode = 0;
             if (string.IsNullOrEmpty(GitHubUsername))
             {
                 return View(model);
             }
 
-            var gitHubClient = new HttpClient();
-            gitHubClient.BaseAddress = new Uri(GITHUBUSERSURL);
-            gitHubClient.DefaultRequestHeaders.Add(USERAGENT, REQUEST);
             HttpResponseMessage response = gitHubClient.GetAsync(GitHubUsername).Result;
 
+            string profileData = "";
             if (response.IsSuccessStatusCode)
             {
-                try
+                profileData = response.Content.ReadAsStringAsync().Result;
+            }
+            else
+            {   /// When API rate limit is exceeded for testing
+                //profileData = "{ \"avatar_url\": \"\", \"login\": \"erikaspo\", \"bio\": \"\", \"company\": \"\", \"location\": \"\" }";
+                //model.StatusCode = 200;
+                model.StatusCode = (int)response.StatusCode;
+                return View(model);
+            }
+
+            try
+            {
+                model = JsonConvert.DeserializeObject<ProfileModel>(profileData);
+                model.StatusCode = (int)response.StatusCode;
+                foreach (var property in model.GetType().GetProperties())
                 {
-                    string profileData = response.Content.ReadAsStringAsync().Result;
-                    model = JsonConvert.DeserializeObject<ProfileModel>(profileData);
-
-                    model.StatusCode = (int)response.StatusCode;
-
-                    foreach (var property in model.GetType().GetProperties())
+                    if (property.GetValue(model, null) == null)
                     {
-                        if (property.GetValue(model, null) == null)
-                        {
-                            property.SetValue(model, EMPTYFIELD);
-                        }
+                        property.SetValue(model, EMPTYFIELD);
                     }
                 }
-                catch (Exception ex)
-                {
-                    Trace.TraceError("Error occured when getting profile from GitHub API." + ex.StackTrace);
-                    return View(new ProfileModel());
-                }
             }
+            catch (Exception ex)
+            {
+                Trace.TraceError("Error occured when getting profile from GitHub API." + ex.StackTrace);
+                return View(model);
+            }
+
             return View(model);
         }
     }
