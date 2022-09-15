13a1082e5c4e1e8a37a6f4cefe7cb8d5653f94ec
ErikasPo <113192441+ErikasPo@users.noreply.github.com>
2022-09-15 17:05:31 +0300

    Delete Strings.cs

--- GitHubApp/Strings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GitHubApp
-{
-    public static class Strings
-    {
-        public static string EmptyField = "This field is empty! Please fill in the field!";
-        public static string Repos = "/repos";
-    }
-
-}
